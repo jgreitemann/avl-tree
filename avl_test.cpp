4d4d53ee8818b4fcf4abbd47a8da7dc79054ec62
Jonas Greitemann <j.greitemann@lmu.de>
2015-12-03 13:48:40 +0100

    Added copy-assignment test

--- avl_test.cpp
+++ avl_test.cpp
@@ -168,6 +168,19 @@ TEST(tree, copy) {
     ASSERT_EQ(true, t != s);
 }
 
+TEST(tree, copy_assign) {
+    avl_tree<double> t;
+    const unsigned N = 10000;
+    random_double_fill(t, N);
+
+    avl_tree<double> s = t;
+    ASSERT_EQ(t, s);
+
+    int r = (int)(s.size() * uniform(rng));
+    s.erase(s.at(r));
+    ASSERT_EQ(true, t != s);
+}
+
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
