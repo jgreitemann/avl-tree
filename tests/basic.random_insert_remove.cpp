a04905156b165b2ebadaab2e7fdafcfb64696371
Jonas Greitemann <j.greitemann@lmu.de>
2015-12-04 10:13:21 +0100

    Changed include path for tests to relative

--- tests/basic.random_insert_remove.cpp
+++ tests/basic.random_insert_remove.cpp
@@ -11,7 +11,7 @@
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU General Public License for more details.
 
-#include "avl_test.hpp"
+#include "../avl_test.hpp"
 
 TEST(basic, random_insert_remove) {
     avl_tree<double> t;
