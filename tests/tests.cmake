24640279a41d4beef2113d1a8fbdb6b8640c2f1b
Jonas Greitemann <j.greitemann@lmu.de>
2016-04-08 17:41:58 +0200

    Switched to using CMake

---
+++ tests/tests.cmake
@@ -0,0 +1,9 @@
+set(AVL_TEST_LIST
+    ${PROJECT_SOURCE_DIR}/tests/basic.random_insert_remove.cpp
+    ${PROJECT_SOURCE_DIR}/tests/iterators.dereference.cpp
+    ${PROJECT_SOURCE_DIR}/tests/iterators.ordering.cpp
+    ${PROJECT_SOURCE_DIR}/tests/tree.clear.cpp
+    ${PROJECT_SOURCE_DIR}/tests/tree.copy_assign.cpp
+    ${PROJECT_SOURCE_DIR}/tests/tree.copy.cpp
+    ${PROJECT_SOURCE_DIR}/tests/tree.equality.cpp
+)
